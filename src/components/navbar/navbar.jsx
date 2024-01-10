4b0c7314b54d268e96804336d6d8347cc422acdb
harshal-ishere <harshal011kamble@gmail.com>
2024-01-10 22:07:33 +0530

    some sgit

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -8,18 +8,17 @@ export default function Navbar() {
     <>
       <section id="Navbar">
         <div className="navbar flex flex-row justify-center py-2">
-          <div className="nav-logo flex flex-row md:px-0 lg:px-5 items-center">
-            <a href="/">
-            <img src={require('../../img/techno_img.png')} />
-            </a>
-            <p className="text-xl sm:text-2xl md:text-3xl  lg:text-5xl">Technovanza</p>
-          </div>
+          <a href="/">
+            <div className="nav-logo flex flex-row md:px-0 lg:px-5 items-center">
+              <img src={require('../../img/techno_img.png')} />
+              <p className="text-xl sm:text-2xl md:text-3xl  lg:text-5xl">Technovanza</p>
+            </div>
+          </a>
           <div className="diagonal-div flex flex-row justify-center items-center">
             <p></p>
           </div>
           <div className="nav-items flex flex-row">
             <div className="nav-elements hidden sm:flex  flex-row justify-center items-center gap-4 md:gap-6 md:text-lg lg:gap-14 lg:text-xl xl:gap-20 xl:text-2xl text-base mx-4">
-              <a href="/"><p>Home</p></a>
               <a href="/events"><p>Events</p></a>
               <a href="/gls"><p>GLS</p></a>
               <a href="/gallery"><p>Gallery</p></a>
@@ -32,13 +31,12 @@ export default function Navbar() {
           </div>
         </div>
       </section>
-      <div style={{zIndex:toggle?10:-10}} className="nav-toggle flex flex-col items-end mt-2">
-        <div className={toggle?" nav-toggle-vis flex flex-col items-start gap-2 mx-6 w-36 sm:hidden ":" inner-nav-toggle flex flex-col items-start gap-2 mx-6 w-36 sm:hidden"}>
-          <a className="flex flex-row justify-around w-36 py-1" href="/"><p>Home</p><AiOutlineArrowRight size={'1.2rem'} /></a>
-          <a className="flex flex-row justify-around w-36 py-1" href="/events"><p>Events</p><AiOutlineArrowRight size={'1.2rem'}/></a>
-          <a className="flex flex-row justify-around w-36 py-1" href="/gls"><p>GLS</p><AiOutlineArrowRight size={'1.2rem'}/></a>
-          <a className="flex flex-row justify-around w-36 py-1" href="/gallery"><p>Gallery</p><AiOutlineArrowRight size={'1.2rem'}/></a>
-          <a className="flex flex-row justify-around w-36 py-1" href="/exhibition"><p>Exhibition</p><AiOutlineArrowRight size={'1.2rem'}/></a>
+      <div style={{ zIndex: toggle ? 10 : -10 }} className="nav-toggle flex flex-col items-end mt-2">
+        <div className={toggle ? " nav-toggle-vis flex flex-col items-start gap-2 mx-6 w-36 sm:hidden " : " inner-nav-toggle flex flex-col items-start gap-2 mx-6 w-36 sm:hidden"}>
+          <a className="flex flex-row justify-around w-36 py-1" href="/events"><p>Events</p><AiOutlineArrowRight size={'1.2rem'} /></a>
+          <a className="flex flex-row justify-around w-36 py-1" href="/gls"><p>GLS</p><AiOutlineArrowRight size={'1.2rem'} /></a>
+          <a className="flex flex-row justify-around w-36 py-1" href="/gallery"><p>Gallery</p><AiOutlineArrowRight size={'1.2rem'} /></a>
+          <a className="flex flex-row justify-around w-36 py-1" href="/exhibition"><p>Exhibition</p><AiOutlineArrowRight size={'1.2rem'} /></a>
         </div>
       </div>
     </>
